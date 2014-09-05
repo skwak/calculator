4e05080fdcf3514be86e4883fd237e10b1fddecf
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-09-04 22:01:20 -0700

    First commit

---
+++ calculator4.rb
@@ -0,0 +1,123 @@
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def exponent(num1, num2)
+  num1 ** num2
+end
+
+def squareroot(num1, num2)
+  Math.sqrt(num1)
+end
+
+puts "Hi, there. What kind of math do you want to do right now?
+  +: Addition
+  -: Subtraction
+  *: Multiplication
+  ^: Exponent
+  √: Find the square root"
+
+calculatron = gets.chomp
+
+if calculatron == "+" || calculatron == "-" || calculatron == "*" ||
+  calculatron == "^"|| calculatron == "√"
+  puts "All right!"
+else
+  abort("I'm sorry. I didn't get that. Exiting the program now.")
+end
+
+if calculatron == "+" || calculatron == "-" || calculatron == "*" || calculatron == "^"
+    puts "What's your first number?"
+    num1 = gets.chomp
+    while num1.to_i.to_s != num1 do
+      puts "Try again"
+      num1 = gets.chomp
+    end
+    num1 = num1.to_i
+
+    puts "Thanks. What's your second number?"
+    num2 = gets.chomp
+    while num2.to_i.to_s != num2 do
+      puts "Try again"
+      num2 = gets.chomp
+    end
+      num2 = num2.to_i
+
+elsif calculatron == "√"
+    puts "What's the number you want the square root of?"
+    num1 = gets.chomp
+      while num1.to_i.to_s != num1 do
+        puts "Try again"
+        num1 = gets.chomp
+      end
+    num1 = num1.to_i
+end
+
+case calculatron
+when "+"
+    puts "#{num1} + #{num2} = #{add(num1, num2)}"
+    puts "Do you want to see the answer again as an
+    1. Integer?
+    2. Float?"
+      int_or_float = gets.chomp
+        if int_or_float == "1"
+          puts "#{add(num1, num2).to_i}"
+        elsif int_or_float == "2"
+          puts "#{add(num1, num2).to_f}"
+        end
+when "-"
+    puts "#{num1} - #{num2} = #{subtract(num1, num2)}"
+    puts "Do you want to see the answer again as an
+    1. Integer
+    2. Float?"
+      int_or_float = gets.chomp
+        if int_or_float == "1"
+          puts "#{subtract(num1, num2).to_i}"
+        elsif int_or_float == "2"
+          puts "#{subtract(num1, num2).to_f}"
+        end
+when "*"
+    puts "#{num1} x #{num2} = #{multiply(num1, num2)}"
+    puts "Do you want to see the answer again as an
+    1. Integer?
+    2. Float?"
+      int_or_float = gets.chomp
+        if int_or_float == "1"
+          puts "#{multiply(num1, num2).to_i}"
+        elsif int_or_float == "2"
+          puts "#{multiply(num1, num2).to_f}"
+        end
+when "^"
+    puts "#{num1}^#{num2} = #{exponent(num1, num2)}"
+    puts "Do you want to see the answer again as an
+    1. Integer?
+    2. Float?"
+      int_or_float = gets.chomp
+        if int_or_float == "1"
+          puts "#{exponent(num1, num2).to_i}"
+        elsif int_or_float == "2"
+          puts "#{exponent(num1, num2).to_f}"
+        end
+when "√"
+    puts "√#{num1} = #{Math.sqrt(num1)}"
+    puts "Do you want to see the answer again as an
+    1. Integer?
+    2. Float?"
+      int_or_float = gets.chomp
+        if int_or_float == "1"
+          puts "#{Math.sqrt(num1).to_i}"
+        elsif int_or_float == "2"
+          puts "#{Math.sqrt(num1).to_f}"
+        end
+
+else
+  puts "Hmm?"
+end
